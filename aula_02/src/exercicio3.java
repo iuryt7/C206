3e424c1d3ad89a7e091ac0a02b4e8f24d6e54ac0
Iury Teixeira <iury.teixeira07@gmail.com>
2024-02-24 19:19:29 -0300

    Modified exercicise 3 (4)

--- aula_02/src/exercicio3.java
+++ aula_02/src/exercicio3.java
@@ -2,41 +2,29 @@ import java.util.Scanner;
 
 public class exercicio3 {
     public static void main(String[] args) {
-
         Scanner entrada = new Scanner(System.in);
 
-        int np1, np2;
+        int alunos;
 
-        System.out.println("Entre com a nota da sua np1: ");
-        np1 = entrada.nextInt();
+        System.out.print("Quantos alunos tem na sala?: ");
+        alunos = entrada.nextInt();
 
-        System.out.println("Entre com a nota da sua np2: ");
-        np2 = entrada.nextInt();
+        switch(alunos){
+            case 10 :
+                System.out.println("Sua sala é a : l-16");
+                break;
 
+            case 20:
+                System.out.println("Sua sala é a : l-16");
+                break;
+            case 30:
+                System.out.println("Sua sala é a : l-22");
+                break;
 
-        int total = np1 + np2;
-        float media = total/2f;
-
-        System.out.println("Media da nota: " + media);
-
-        if(media >=  60){
-            System.out.println("Você passou!");
-        }
-        else{
-            System.out.println("Você precisa fazer a np3!");
-            int np3;
-            System.out.println("Entre com a nota da sua np3: ");
-            np3 = entrada.nextInt();
-            float mediaf = np3 + media;
-            if(mediaf >= 50){
-                System.out.println("Você passou!");
-            }
-            else{
-                System.out.println("Você não passou!");
-            }
+            default:
+                System.out.println("Digite um valor correto");
         }
 
         entrada.close();
-
     }
 }
